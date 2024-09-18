3633cba73737595758d3b6747904a908d3900863
Vivek Singh <142485319+vivek7156@users.noreply.github.com>
2024-09-18 15:39:33 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <h1 className="navbar-title">Expense Tracker</h1>
+    </div>
+  )
+}
+
+export default Header
